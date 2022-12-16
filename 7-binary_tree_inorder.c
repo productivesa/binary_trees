f2c9f8324cc9a63a110a23ca256f152f58a8534a
productivesa <salahzeinu@gmail.com>
2022-12-16 10:55:22 -0800

    7-binary_tree_inorder.c

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -7,7 +7,7 @@
  * @func: pointer to a function to call for each node
  * Return: no return
  */
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 	{
@@ -15,8 +15,8 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	}
 	else
 	{
+		binary_tree_inorder(tree->left, func);
 		func(tree->n);
-		binary_tree_preorder(tree->left, func);
-		binary_tree_preorder(tree->right, func);
+		binary_tree_inorder(tree->right, func);
 	}
 }
