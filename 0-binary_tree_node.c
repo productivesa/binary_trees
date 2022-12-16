cdb94e8873268a010a38c80301f97bfdfbcc2b8e
productivesa <salahzeinu@gmail.com>
2022-12-16 08:15:10 -0800

    0-binary_tree_node.c second clone

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,21 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - create binary code
- * @parent: parent node
- * @value: node of value
- * Return: new node pointer or null
+ * binary_tree_node - Creates a binary tree mode
+ * @parent: node parent
+ * @value: node value
+ * Return: pointer to the new node or NULL on failure
  */
-binary_tree_t 8binary_tree_node(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node_n;
 
 	node_n = malloc(sizeof(binary_tree_t));
 	if (node_n == NULL)
-	{
 		return (NULL);
-	}
-	node_n->parent  = parent;
+
+	node_n->parent = parent;
 	node_n->n = value;
 	node_n->left = NULL;
 	node_n->right = NULL;
